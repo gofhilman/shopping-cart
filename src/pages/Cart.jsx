b2956b21dde5694d4bfe50d57b05dd2fa59ae55d
Hilman Fikry <hilman12fikry@gmail.com>
2025-08-06 10:59:54 +0700

    feat: complete all app features

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,24 +1,71 @@
 import CartCard from "@/components/CartCard";
-import { useOutletContext } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import formatRupiah from "@/lib/format-rupiah";
+import { Link, useOutletContext } from "react-router-dom";
 
 export default function Cart() {
   const { cart, setCart } = useOutletContext();
+  const totalPrice = cart.reduce(
+    (acc, item) => acc + item.quantity * item.price,
+    0,
+  );
 
   return (
     <article>
       <h2>Your Shopping Cart</h2>
       <div>
         <div>
-          {cart.map((product) => (
-            <CartCard
-              key={product.id}
-              product={product}
-              cart={cart}
-              setCart={setCart}
-            />
-          ))}
+          {cart.length === 0 ? (
+            <div>
+              <p>Your cart is empty.</p>
+              <Button>
+                <Link to="/shop">Continue shopping</Link>
+              </Button>
+            </div>
+          ) : (
+            cart.map((product) => (
+              <CartCard
+                key={product.id}
+                product={product}
+                cart={cart}
+                setCart={setCart}
+              />
+            ))
+          )}
         </div>
-        <div></div>
+        <Card>
+          <CardHeader>
+            <CardHeader>
+              <CardTitle>Order summary</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div>
+                <p>Subtotal</p>
+                <p>{formatRupiah(totalPrice)}</p>
+              </div>
+              <div>
+                <p>Shipping</p>
+                <p>Free</p>
+              </div>
+              <Separator />
+              <div>
+                <p>Total</p>
+                <p>{formatRupiah(totalPrice)}</p>
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Proceed to checkout</Button>
+            </CardFooter>
+          </CardHeader>
+        </Card>
       </div>
     </article>
   );
